46012cc37b02a7e1fd95e671d33b1b38397c023d
Dmitrii Titarenko <padrition@gmail.com>
2024-12-07 20:06:09 +0100

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,101 @@
+import java.util.Properties
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    kotlin("kapt")
+    alias(libs.plugins.com.google.dagger.hilt.android)
+    alias(libs.plugins.com.google.devtools.ksp)
+}
+
+val properties = Properties()
+properties.load(project.rootProject.file("local.properties").reader())
+
+
+android {
+    namespace = "cz.mendelu.projek"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "cz.mendelu.projek"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            buildConfigField("String", "BASE_URL", properties.getProperty("baseurlproduction"))
+        }
+        debug {
+            buildConfigField("String", "BASE_URL", properties.getProperty("baseurldevel"))
+        }
+
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Hilt
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.compose)
+    kapt(libs.hilt.kapt)
+
+    //Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.okhtt3)
+    implementation(libs.retrofit.moshi)
+
+    //Moshi
+    implementation(libs.moshi)
+    implementation(libs.moshi.kotlin)
+    ksp(libs.moshi.codegen)
+
+    // Navigation
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ktx)
+    implementation(libs.navigation.compose)
+
+    implementation(libs.lifecycle)
+    implementation(libs.room.ktx)
+    implementation(libs.room.viewmodel)
+    implementation(libs.room.lifecycle)
+    implementation(libs.room.runtime)
+    kapt(libs.room.compiler.kapt)
+}
